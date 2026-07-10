13c4a434148826e9458a191016a60b0da4b52b40
TusharW4ni <reachtusharwani@gmail.com>
2026-07-10 14:04:53 -0500

    switch: generation 17 (2026-07-10 14:04)

--- home/tushar.nix
+++ home/tushar.nix
@@ -16,7 +16,7 @@ let
     if ! git diff --staged --quiet; then
       GENERATION=$(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -1 | awk '{print $1}')
       git commit -m "switch: generation $GENERATION ($(date '+%Y-%m-%d %H:%M'))"
-      git push origin master
+      git push origin main
     else
       echo "nixos-switch: nothing new to commit"
     fi
